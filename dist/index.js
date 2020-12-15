379f30b49869b6e5e743c66e1c5959b3fb69b9fc
Caubert Castro <caubert@bempromotora.com.br>
2020-12-15 17:00:22 -0300

    Adicionado algumas configurações do babel e ponto de entrada do teste

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return /*#__PURE__*/React.createElement("div", {
+    style: {
+      boxSizing: "border-box",
+      width: "90%",
+      height: "auto",
+      textAlign: "center",
+      border: "1px solid #ccc",
+      margin: "20px auto"
+    }
+  }, /*#__PURE__*/React.createElement("h1", null, "Componente compartilhado entre aplica\xE7\xF5es"));
+};
+
+export default App;
---
+++ src/component/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        boxSizing: "border-box",
+        width: "90%",
+        height: "auto",
+        textAlign: "center",
+        border: "1px solid #ccc",
+        margin: "20px auto",
+      }}
+    >
+      <h1>Componente compartilhado entre aplicações</h1>
+    </div>
+  );
+};
+
+export default App;
